12805709a5f14d4c9aeada08a814cda429b7a681
Zarebski <Alex@Alex-Zarebskis-MacBook-Pro.local>
2015-05-07 17:32:19 +1000

    wrote this using some numpy functions to avoid a for loop

--- bpath1.py
+++ bpath1.py
@@ -1,14 +1,27 @@
 import numpy as np 
+import matplotlib.pyplot as plt
+
+def bm():
+    """Brownian path simulation"""
+    np.random.seed(100)    # set the seed of the random number generator
+    stopping_time = 1.
+    N = 10
+    dt = stopping_time/N 
+
+    dW = np.sqrt(dt)*np.random.normal(size=N)
+    W = np.cumsum(dW)
+    W = np.hstack(([0],W))
+    
+    T = np.linspace(0,stopping_time,N+1)
+    
+    return T,W
 
 def main():
-	"""Brownian path simulation"""
-	np.random.seed(100)		# set the seed of the random number generator
-	T = 1. 					
-	N = 500					
-	dt = T/N 				
-
-	dW = np.zeros(N) 		# preallocate arrays ...
-	W = np.zeros(N)			# for efficiency
+    T,W = bm()
+    
+    plt.figure()
+    plt.plot(T,W)
+    plt.show()
 
 if __name__ == '__main__':
 	main()
